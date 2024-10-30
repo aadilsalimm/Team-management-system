0d7c72638856b63873aa261f2fe8be2152a31157
aadilsalimm <130083334+aadilsalimm@users.noreply.github.com>
2024-10-31 00:31:35 +0530

    Set isAuthenticated as common middleware for all routes in admin-home

--- routes/admin/admin-home.js
+++ routes/admin/admin-home.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 const isAuthenticated = require('../../authenticate');
 
-router.get('/', isAuthenticated, (req, res) => {
+router.use(isAuthenticated);
+
+router.get('/', (req, res) => {
     res.render('admin/admin-home');
 });
 
